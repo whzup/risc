a6ad8547c36d47ef5eaddd57c2ae6292175ce6d2
Aaron Moser <39431903+whzup@users.noreply.github.com>
2019-01-14 08:09:53 +0100

    Correct errors in barrel shifter

--- src/alu/shifter.vhd
+++ src/alu/shifter.vhd
@@ -5,7 +5,8 @@ use ieee.numeric_std.all;
 entity mux is
     port
     (
-    i_sig : in std_logic_vector(1 downto 0);
+    i_sig0 : in std_logic;
+    i_sig1 : in std_logic;
     i_sel : in std_logic;
     o_sig : out std_logic
     );
@@ -13,8 +14,8 @@ end entity;
 
 architecture behaviour of mux is
 begin
-    o_sig <= i_sig(0) when i_sel='0' else
-             i_sig(1) when i_sel='1';
+    o_sig <= i_sig0 when i_sel='0' else
+             i_sig1 when i_sel='1';
 end architecture;
 
 library ieee;
@@ -36,7 +37,8 @@ architecture behaviour of shifter is
     component mux is
         port
         (
-        i_sig : in std_logic_vector(15 downto 0);
+        i_sig0 : in std_logic;
+        i_sig1 : in std_logic;
         i_sel : in std_logic;
         o_sig : out std_logic
         );
@@ -52,11 +54,12 @@ architecture behaviour of shifter is
     signal s_l3: std_logic_vector(15 downto 0);
 begin
 
-    gen_layer0_mux : for i in 15 to 0 generate
+    gen_layer0_mux : for i in 0 to 15 generate
         mux_lower8 : if i <= 7 generate
             L8 : mux port map
             (
-            i_sig => '0' & i_sig(i),
+            i_sig0 => i_op(i),
+            i_sig1 => '0',
             i_sel => i_s(3),
             o_sig => s_l0(i)
             );
@@ -64,18 +67,20 @@ begin
         mux_upper8 : if i > 7 generate
             U8 : mux port map
             (
-            i_sig => i_sig(i-8) & i_sig(i),
+            i_sig0 => i_op(i),
+            i_sig1 => i_op(i-8),
             i_sel => i_s(3),
             o_sig => s_l0(i)
             );
         end generate;
     end generate;
 
-    gen_layer1_mux : for i in 15 to 0 generate
+    gen_layer1_mux : for i in 0 to 15 generate
         mux_lower4 : if i <= 3 generate
             L4 : mux port map
             (
-            i_sig => '0' & s_l0(i),
+            i_sig0 => s_l0(i),
+            i_sig1 => '0',
             i_sel => i_s(2),
             o_sig => s_l1(i)
             );
@@ -83,18 +88,20 @@ begin
         mux_upper12 : if i > 3 generate
             U12 : mux port map
             (
-            i_sig => s_l0(i-4) & s_l0(i),
+            i_sig0 => s_l0(i),
+            i_sig1 => s_l0(i-4),
             i_sel => i_s(2),
             o_sig => s_l1(i)
             );
         end generate;
     end generate;
 
-    gen_layer2_mux : for i in 15 to 0 generate
+    gen_layer2_mux : for i in 0 to 15 generate
         mux_lower2 : if i <= 1 generate
             L2 : mux port map
             (
-            i_sig => '0' & s_l1(i),
+            i_sig0 => s_l1(i),
+            i_sig1 => '0',
             i_sel => i_s(1),
             o_sig => s_l2(i)
             );
@@ -102,18 +109,20 @@ begin
         mux_upper14 : if i > 1 generate
             U14 : mux port map
             (
-            i_sig => s_l1(i-2) & s_l1(i),
+            i_sig0 => s_l1(i),
+            i_sig1 => s_l1(i-2),
             i_sel => i_s(1),
             o_sig => s_l2(i)
             );
         end generate;
     end generate;
      
-    gen_layer3_mux : for i in 15 to 0 generate
+    gen_layer3_mux : for i in 0 to 15 generate
         mux_lower1 : if i = 0 generate
             L1 : mux port map
             (
-            i_sig => '0' & s_l2(i),
+            i_sig0 => s_l2(i),
+            i_sig1 => '0',
             i_sel => i_s(0),
             o_sig => s_l3(i)
             );
@@ -121,23 +130,13 @@ begin
         mux_upper15 : if i > 0 generate
             U15 : mux port map
             (
-            i_sig => s_l2(i-1) & s_l2(i),
+            i_sig0 => s_l2(i),
+            i_sig1 => s_l2(i-1),
             i_sel => i_s(0),
             o_sig => s_l3(i)
             );
         end generate;
     end generate;
-
-    -- shift_proc : process(all)
-    -- begin
-    --     if i_ctrl='0' then
-    --         carry <= i_op(15);
-    --         shift <= std_logic_vector(shift_left(unsigned(i_op), to_integer(unsigned(i_s))));
-    --     else
-    --         carry <= i_op(0);
-    --         shift <= std_logic_vector(shift_right(unsigned(i_op), to_integer(unsigned(i_s))));
-    --     end if;
-    -- end process;
 
     o_r <= s_l3;
     o_c <= '0';
