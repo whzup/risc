6f9a8ea06b5e388825cba06a3ba482839e5af3a3
whzup <39431903+whzup@users.noreply.github.com>
2019-01-16 16:42:17 +0100

    Refactor shifter

--- src/alu/shifter.vhd
+++ src/alu/shifter.vhd
@@ -7,6 +7,7 @@
 -- Date:        14.01.2019
 ------------------------------------------------------------
 
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -32,6 +33,7 @@ begin
              i_sig1 when i_sel='1';
 end architecture;
 
+
 -- Shifter
 library ieee;
 use ieee.std_logic_1164.all;
@@ -49,16 +51,31 @@ entity shifter is
     o_f:   out std_logic; -- overflow flag
     o_z:   out std_logic  -- zero flag
     );
+
+    function reverse_vector(a: in std_logic_vector)
+    return std_logic_vector is variable res : std_logic_vector(a'range);
+        alias aa: std_logic_vector(a'reverse_range) is a;
+    begin
+        for i in aa'range loop
+            res(i) := aa(i);
+        end loop;
+        return res;
+    end function;
+
 end entity;
 
 architecture behaviour of shifter is
     component mux is
+        generic
+        (
+        width : natural
+        );
         port
         (
-        i_sig0: in std_logic;
-        i_sig1: in std_logic;
+        i_sig0: in std_logic_vector(width-1 downto 0);
+        i_sig1: in std_logic_vector(width-1 downto 0);
         i_sel:  in std_logic;
-        o_sig:  out std_logic
+        o_sig:  out std_logic_vector(width-1 downto 0)
         );
     end component;
 
@@ -67,6 +84,9 @@ architecture behaviour of shifter is
 
     -- Reverse signals
     signal rev_0: std_logic_vector(15 downto 0);
+    signal rev_0_8: std_logic_vector(7 downto 0);
+    signal rev_0_4: std_logic_vector(3 downto 0);
+    signal rev_0_2: std_logic_vector(1 downto 0);
     signal rev_1: std_logic_vector(15 downto 0);
     
     -- Rotation signals
@@ -83,6 +103,9 @@ architecture behaviour of shifter is
 
     -- Arithmetic shifting
     signal s : std_logic;
+    signal s_2 : std_logic_vector(1 downto 0);
+    signal s_4 : std_logic_vector(3 downto 0);
+    signal s_8 : std_logic_vector(7 downto 0);
     signal s_tmp : std_logic;
     signal sl : std_logic;
     signal sr : std_logic;
@@ -93,9 +116,16 @@ architecture behaviour of shifter is
     signal of_mux_out2 : std_logic_vector(1 downto 0);
     signal of_mux_out3 : std_logic;
 begin
+    s_8 <= (others => s);
+    s_4 <= (others => s);
+    s_2 <= (others => s);
 
     sl <= i_dir and i_ar;
     sr <= not i_dir and i_ar;
+
+    rev_0_8 <= (others => rev_0(0));
+    rev_0_4 <= (others => rev_0(0));
+    rev_0_2 <= (others => rev_0(0));
 
     -- Generate Multiplexer layers
     -- Reverse the bit order for a right shift
@@ -107,7 +137,7 @@ begin
     port map
         (
         i_sig0 => i_op,
-        i_sig1 => i_op(0 to 15),
+        i_sig1 => reverse_vector(i_op),
         i_sel => i_dir,
         o_sig => rev_0
         );
@@ -120,10 +150,10 @@ begin
         )
     port map
         (
-        i_sig1 => '0',
-        i_sig0 => rev_0(15),
+        i_sig1(0) => '0',
+        i_sig0(0) => rev_0(15),
         i_sel => sr,
-        o_sig => s
+        o_sig(0) => s
         );
 
     -- Zeroth rotation layer
@@ -134,7 +164,7 @@ begin
         )
     port map
         (
-        i_sig0 => s,
+        i_sig0 => s_8,
         i_sig1 => rev_0(15 downto 8),
         i_sel => i_rot,
         o_sig => rot_0(15 downto 8)
@@ -151,7 +181,7 @@ begin
     port map
         (
         i_sig0 => rot_0(7 downto 0),
-        i_sig1 => '0',
+        i_sig1 => "00000000",
         i_sel  => i_s(3),
         o_sig  => s_l0(7 downto 0)
         );
@@ -177,7 +207,7 @@ begin
         )
     port map
         (
-        i_sig0 => (others => s),
+        i_sig0 => s_4,
         i_sig1 => s_l0(15 downto 12),
         i_sel => i_rot,
         o_sig => rot_1(15 downto 12)
@@ -194,7 +224,7 @@ begin
     port map
         (
         i_sig0 => rot_1(3 downto 0),
-        i_sig1 => '0',
+        i_sig1 => "0000",
         i_sel  => i_s(2),
         o_sig  => s_l1(3 downto 0)
         );
@@ -220,7 +250,7 @@ begin
         )
     port map
         (
-        i_sig0 => s,
+        i_sig0 => s_2,
         i_sig1 => s_l1(15 downto 14),
         i_sel => i_rot,
         o_sig => rot_2(15 downto 14)
@@ -228,132 +258,156 @@ begin
 
     rot_2(13 downto 0) <= s_l1(13 downto 0);
 
-    gen_layer2_mux : for i in 0 to 15 generate
         -- 2-bit shift
-        mux_lower2 : if i <= 1 generate
-            L2 : mux
-            generic map
-                (
-                width => 2
-                )
-            port map
-                (
-                i_sig0 => rot_2(1 downto 0),
-                i_sig1 => '0',
-                i_sel  => i_s(1),
-                o_sig  => s_l2(1 downto 0)
-                );
-        end generate;
-        mux_upper14 : if i > 1 generate
-            U14 : mux port map
-            (
-            i_sig0 => rot_2(i),
-            i_sig1 => rot_2(i-2),
-            i_sel  => i_s(1),
-            o_sig  => s_l2(i)
-            );
-        end generate;
-    end generate;
-
-    gen_rot_layer3 : for i in 0 to 1 generate
-        -- Third rotation layer
-        ROT : mux port map
+    mux_lower2 : mux
+    generic map
         (
-        i_sig0 => s,
-        i_sig1 => s_l2(i+14),
+        width => 2
+        )
+    port map
+        (
+        i_sig0 => rot_2(1 downto 0),
+        i_sig1 => "00",
+        i_sel  => i_s(1),
+        o_sig  => s_l2(1 downto 0)
+        );
+
+    mux_upper14 : mux
+    generic map
+        (
+        width => 14
+        )
+    port map
+        (
+        i_sig0 => rot_2(15 downto 2),
+        i_sig1 => rot_2(13 downto 0),
+        i_sel  => i_s(1),
+        o_sig  => s_l2(15 downto 2)
+        );
+
+    -- Third rotation layer
+    rot_l3_mux : mux
+    generic map
+        (
+        width => 1
+        )
+    port map
+        (
+        i_sig0(0) => s,
+        i_sig1(0) => s_l2(15),
         i_sel => i_rot,
-        o_sig => rot_3(i+14)
+        o_sig(0) => rot_3(15)
         );
-    end generate;
 
-    rot_layer3_proc : process(all)
-        -- Fill the rotation vector
-    begin
-        for i in 0 to 14 loop
-            rot_3(i) <= s_l2(i);
-        end loop;
-    end process;
+        rot_3(15) <= s_l2(15);
      
-    gen_layer3_mux : for i in 0 to 15 generate
         -- 1-bit shift
-        mux_lower1 : if i = 0 generate
-            L1 : mux port map
-            (
-            i_sig0 => rot_3(i),
-            i_sig1 => '0',
-            i_sel  => i_s(0),
-            o_sig  => s_tmp
-            );
-        end generate;
-        mux_upper15 : if i > 0 generate
-            U15 : mux port map
-            (
-            i_sig0 => rot_3(i),
-            i_sig1 => rot_3(i-1),
-            i_sel  => i_s(0),
-            o_sig  => s_l3(i)
-            );
-        end generate;
-    end generate;
-
-    sla_mux : mux port map
-    -- Arithmetic left shifting
-    (
-    i_sig0 => s_tmp,
-    i_sig1 => rev_0(0),
-    i_sel => sl,
-    o_sig => s_l3(0)
-    );
-
-    gen_rev1_mux : for i in 0 to 15 generate
-        -- Reverse the bit order again
-        rev1_mux : mux port map
+    mux_lower1 : mux
+    generic map
         (
-        i_sig0 => s_l3(i),
-        i_sig1 => s_l3(15-i),
-        i_sel => i_dir,
-        o_sig => rev_1(i)
+        width => 1
+        )
+    port map
+        (
+        i_sig0(0) => rot_3(0),
+        i_sig1(0) => '0',
+        i_sel  => i_s(0),
+        o_sig(0)  => s_tmp
         );
-    end generate;
+
+    mux_upper15 : mux
+    generic map
+        (
+        width => 15
+        )
+    port map
+        (
+        i_sig0 => rot_3(15 downto 1),
+        i_sig1 => rot_3(14 downto 0),
+        i_sel  => i_s(0),
+        o_sig  => s_l3(15 downto 1)
+        );
+
+    sla_mux : mux
+    -- Arithmetic left shifting
+    generic map
+        (
+        width => 1
+        )
+    port map
+        (
+        i_sig0(0) => s_tmp,
+        i_sig1(0) => rev_0(0),
+        i_sel => sl,
+        o_sig(0) => s_l3(0)
+        );
+
+    -- Reverse the bit order again
+    rev1_mux : mux
+    generic map
+        (
+        width => 16
+        )
+    port map
+        (
+        i_sig0 => s_l3(15 downto 0),
+        i_sig1 => reverse_vector(s_l3),
+        i_sel => i_dir,
+        o_sig => rev_1(15 downto 0)
+        );
 
     -- Overflow detection
-    gen_overflow0_mux : for i in 0 to 7 generate
-        of_mux : mux port map
+    of0_mux : mux
+    generic map
         (
-        i_sig0 => rev_0(0),
-        i_sig1 => rev_0(i+1),
+        width => 8
+        )
+    port map
+        (
+        i_sig0 => rev_0_8,
+        i_sig1 => rev_0(8 downto 1),
         i_sel => i_s(3),
-        o_sig => of_mux_out0(i)
+        o_sig => of_mux_out0(7 downto 0)
         );
-    end generate;
 
-    gen_overflow1_mux : for i in 0 to 3 generate
-        of_mux : mux port map
+    of1_mux : mux
+    generic map
         (
-        i_sig0 => rev_0(0),
-        i_sig1 => s_l0(i+1),
+        width => 4
+        )
+    port map
+        (
+        i_sig0 => rev_0_4,
+        i_sig1 => s_l0(4 downto 1),
         i_sel => i_s(2),
-        o_sig => of_mux_out1(i)
+        o_sig => of_mux_out1(3 downto 0)
         );
-    end generate;
 
-    gen_overflow2_mux : for i in 0 to 1 generate
-        of_mux : mux port map
+    of2_mux : mux
+    generic map
         (
-        i_sig0 => rev_0(0),
-        i_sig1 => s_l1(i+1),
+        width => 2
+        )
+    port map
+        (
+        i_sig0 => rev_0_2,
+        i_sig1 => s_l1(2 downto 1),
         i_sel => i_s(1),
-        o_sig => of_mux_out2(i)
+        o_sig => of_mux_out2(1 downto 0)
         );
-    end generate;
 
-    overflow3_mux : mux port map
-    (
-    i_sig0 => rev_0(0),
-    i_sig1 => s_l2(1),
-    i_sel => i_s(0),
-    o_sig => of_mux_out3
-    );
+    of3_mux : mux
+    generic map
+        (
+        width => 1
+        )
+    port map
+        (
+        i_sig0(0) => rev_0(0),
+        i_sig1(0) => s_l2(1),
+        i_sel => i_s(0),
+        o_sig(0) => of_mux_out3
+        );
 
     overflow_proc : process(all)
         variable xor_out0 : std_logic_vector(7 downto 0);
