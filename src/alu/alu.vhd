03abd42921b409b6bcca9b20b82bc3eda2b24617
whzup <39431903+whzup@users.noreply.github.com>
2019-01-24 14:44:14 +0100

    Add component instances in the ALU

--- src/alu/alu.vhd
+++ src/alu/alu.vhd
@@ -17,7 +17,8 @@ entity alu is
     o_res: out std_logic_vector(15 downto 0);
     o_z:   out std_logic;
     o_f:   out std_logic;
-    o_c:   out std_logic
+    o_c:   out std_logic;
+    o_n:   out std_logic
     ); 
 end entity;
 
@@ -64,6 +65,59 @@ architecture behaviour of alu is
         o_f:   out std_logic;
         o_z:   out std_logic
         );
-    end component
+    end component;
+    
+    -- Adder signals
+    signal inv_vec:  std_logic_vector(15 downto 0);
+    signal sub_flag: std_logic;
+    signal add_res:  std_logic_vector(15 downto 0);
+    signal add_c:    std_logic;
+
+    -- LU signals
+    signal lu_res: std_logic_vector(15 downto 0);
+    signal lu_z:   std_logic;
+
+    -- Shifter signals
+    signal shift : std_logic_vector(3 downto 0);
+    signal sh_res: std_logic_vector(15 downto 0);
+    signal sh_f:   std_logic;
+    signal sh_z:   std_logic;
+
 begin
+    -- Instantiate the components
+    claa_inst : claa_16bit port map
+    (
+    i_opa => i_op1,
+    i_opb => inv_vec,
+    i_c   => sub_flag,
+    o_e   => add_res,
+    o_p   => open,
+    o_g   => open,
+    o_c   => add_c
+    );
+
+    lu_int : lu port map
+    (
+    i_op1  => i_op1,
+    i_op2  => i_op2,
+    i_and  => open,
+    i_or   => open,
+    i_xor  => open,
+    i_not  => open,
+    o_res  => lu_res,
+    o_z    => lu_z
+    );
+
+    shifter_inst : shifter port map
+    (
+    i_op  => i_op1,
+    i_s   => shift,
+    i_dir => open,
+    i_rot => open,
+    i_ar  => open,
+    o_r   => sh_res,
+    o_f   => sh_f,
+    o_z   => sh_z
+    );
+
 end architecture;
