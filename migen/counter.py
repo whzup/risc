b05465e47b666225c9850d945401ee90953570bd
whzup <39431903+whzup@users.noreply.github.com>
2019-01-28 17:12:00 +0100

    Remove migen files

--- migen/counter.py
+++
@@ -1,9 +0,0 @@
-from migen import Module, Signal, If
-
-
-class Counter(Module):
-    def __init__(self):
-        self.i_ce = Signal()
-        self.o_count = Signal(16)
-
-        self.sync += If(self.i_ce, self.o_count.eq(self.o_count + 1))
